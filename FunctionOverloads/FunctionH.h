6fba2817e789867e0d6337c809de79c771682f5c
SSV <lineuvepro@gmail.com>
2021-01-30 11:51:14 +0300

    Separation DONE

---
+++ FunctionOverloads/FunctionH.h
@@ -0,0 +1,57 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+#define delimiter "\n-------------------------------------------\n"
+
+const int ROWS = 8;
+const int COLS = 5;
+
+void FillRand(int arr[], const int n);
+void FillRand(double arr[], const int n);
+void FillRand(char arr[], const int n);
+void FillRand(int arr[ROWS][COLS], const int m, const int n);
+void FillRand(double arr[ROWS][COLS], const int m, const int n);
+void FillRand(char arr[ROWS][COLS], const int m, const int n);
+
+void Print(int arr[], const int n);
+void Print(double arr[], const int n);
+void Print(char arr[], const int n);
+void Print(int arr[ROWS][COLS], const int m, const int n);
+void Print(double arr[ROWS][COLS], const int m, const int n);
+void Print(char arr[ROWS][COLS], const int m, const int n);
+
+void Sort(int arr[], const int n);
+void Sort(double arr[], const int n);
+void Sort(char arr[], const int n);
+void Sort(int arr[ROWS][COLS], const int m, const int n);
+void Sort(double arr[ROWS][COLS], const int m, const int n);
+void Sort(char arr[ROWS][COLS], const int m, const int n);
+
+int Sum(int arr[], const int n);
+double Sum(double arr[], const int n);
+int Sum(char arr[], const int n);
+int Sum(int arr[ROWS][COLS], const int m, const int n);
+double Sum(double arr[ROWS][COLS], const int m, const int n);
+int Sum(char arr[ROWS][COLS], const int m, const int n);
+
+double Avg(int arr[], const int n);
+double Avg(double arr[], const int n);
+double Avg(char arr[], const int n);
+double Avg(int arr[ROWS][COLS], const int m, const int n);
+double Avg(double arr[ROWS][COLS], const int m, const int n);
+double Avg(char arr[ROWS][COLS], const int m, const int n);
+
+int minValueIn(int arr[], const int n);
+double minValueIn(double arr[], const int n);
+char minValueIn(char arr[], const int n);
+int minValueIn(int arr[ROWS][COLS], const int m, const int n);
+double minValueIn(double arr[ROWS][COLS], const int m, const int n);
+char minValueIn(char arr[ROWS][COLS], const int m, const int n);
+
+int maxValueIn(int arr[], const int n);
+double maxValueIn(double arr[], const int n);
+char maxValueIn(char arr[], const int n);
+int maxValueIn(int arr[ROWS][COLS], const int m, const int n);
+double maxValueIn(double arr[ROWS][COLS], const int m, const int n);
+char maxValueIn(char arr[ROWS][COLS], const int m, const int n);
